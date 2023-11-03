c0133de92a1f6945771d6f1a7e6b4079b7db6666
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-03 22:57:59 +0600

    firebase config,and data set

---
+++ src/Data/Data.ts
@@ -0,0 +1,18 @@
+type ImageItem = {
+	image: string;
+	selected: boolean;
+};
+//data for show
+export const data: ImageItem[] = [
+	{ image: "/images/image-1.webp", selected: false },
+	{ image: "/images/image-2.webp", selected: false },
+	{ image: "/images/image-3.webp", selected: false },
+	{ image: "/images/image-4.webp", selected: false },
+	{ image: "/images/image-5.webp", selected: false },
+	{ image: "/images/image-6.webp", selected: false },
+	{ image: "/images/image-7.webp", selected: false },
+	{ image: "/images/image-8.webp", selected: false },
+	{ image: "/images/image-9.webp", selected: false },
+	{ image: "/images/image-10.jpeg", selected: false },
+	{ image: "/images/image-11.jpeg", selected: false },
+];
---
+++ src/Firebase/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBbOBNO7Xmml44oY-QvDc_m0oDYBAmDDx4",
+	authDomain: "image-gallery-bc2a6.firebaseapp.com",
+	projectId: "image-gallery-bc2a6",
+	storageBucket: "image-gallery-bc2a6.appspot.com",
+	messagingSenderId: "259346912350",
+	appId: "1:259346912350:web:1b38d2bd45d805b48810cf",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
