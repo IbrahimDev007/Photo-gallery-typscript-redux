531558e6ec72286d6e389343efaa4b2f7f266804
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-03 23:34:36 +0600

    provider use redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import AuthProvider from "./Provider/Provider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const store = configureStore({
+	reducer: {
+		images: "",
+	},
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<AuthProvider>
+			{" "}
+			{/* //auth provider use authintication */}
+			<Provider store={store}>
+				{" "}
+				{/* //redux provider use  store data and dispatch */}
+				<App />
+			</Provider>
+		</AuthProvider>
+	</React.StrictMode>
+);
