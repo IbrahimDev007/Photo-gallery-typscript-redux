328eef2274721451b15212363015598d25d1ba5a
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-03 23:19:43 +0600

    auth provider add

--- src/Provider/Provider.tsx
+++ src/Provider/Provider.tsx
@@ -1,12 +1,21 @@
-import { createContext, useState, ReactNode } from "react";
-import { User, getAuth } from "firebase/auth";
+import { createContext, useEffect, useState, ReactNode } from "react";
+import {
+	GoogleAuthProvider,
+	User,
+	getAuth,
+	onAuthStateChanged,
+	signInWithPopup,
+	signOut,
+	updateProfile,
+} from "firebase/auth";
 import { app } from "../Firebase/firebase.config";
 
 export type AuthContextType = {
 	user: User | null;
 	loading: boolean;
-	// googleSignIn: () => Promise<void>;
-	// logOut: () => Promise<void>;
+	googleSignIn: () => Promise<void>;
+	logOut: () => Promise<void>;
+	updateUserProfile: (name: string, photo: string) => Promise<void>;
 };
 
 export const AuthContext = createContext<AuthContextType | null>(null); //export auth context to use everywher by useauth context
@@ -20,9 +29,47 @@ type AuthProviderProps = {
 const AuthProvider = ({ children }: AuthProviderProps) => {
 	const [user, setUser] = useState<User | null>(null);
 	const [loading, setLoading] = useState<boolean>(false);
+
+	//provide google
+
+	const googleProvider = new GoogleAuthProvider();
+	//google promise for login by firebase
+	const googleSignIn = () => {
+		setLoading(true);
+		return signInWithPopup(auth, googleProvider);
+	};
+	//google promise for logout by firebase
+	const logOut = () => {
+		setLoading(false);
+		setUser(null);
+		return signOut(auth);
+	};
+	// update profile for data future uses and store firebase
+	const updateUserProfile = (name: string, photo: string) => {
+		return updateProfile(auth.currentUser, {
+			displayName: name,
+			photoURL: photo,
+			role: "user",
+		});
+	};
+	//midalware firebase authstate change
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+			console.log("current user", currentUser, currentUser?.email);
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	}, []);
+	//authinfo  data to pass provider
 	const authInfo: AuthContextType = {
 		user,
 		loading,
+		googleSignIn,
+		logOut,
+		updateUserProfile,
 	};
 
 	return (
